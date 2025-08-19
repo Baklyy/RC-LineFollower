1cac5deda117e9de9ad8f8e964850801e0a91313
George_Essmat <125737319+George2251@users.noreply.github.com>
2025-08-19 12:44:46 +0300

    Delete AutonomousRC_Project directory

--- AutonomousRC_Project/Debug/library/LIB/delay.d
+++
@@ -1,5 +0,0 @@
-library/LIB/delay.o library/LIB/delay.o:  \
- E:/ITI\ -\ Embedded\ Systems/myEmbeddedFiles/library/LIB/delay.c \
-  E:/ITI\ -\ Embedded\ Systems/myEmbeddedFiles/library/LIB/STD_TYPES.h
-
-E:/ITI\ -\ Embedded\ Systems/myEmbeddedFiles/library/LIB/STD_TYPES.h:
--- AutonomousRC_Project/Debug/main.d
+++
@@ -1,7 +0,0 @@
-main.o main.o: ../main.c \
-  E:\ITI\ -\ Embedded\ Systems\myEmbeddedFiles\library/LIB/STD_TYPES.h \
-  E:\ITI\ -\ Embedded\ Systems\myEmbeddedFiles\library/APP/RC_SYSTEM/RC_SYSTEM_interface.h
-
-E:\ITI\ -\ Embedded\ Systems\myEmbeddedFiles\library/LIB/STD_TYPES.h:
-
-E:\ITI\ -\ Embedded\ Systems\myEmbeddedFiles\library/APP/RC_SYSTEM/RC_SYSTEM_interface.h:
