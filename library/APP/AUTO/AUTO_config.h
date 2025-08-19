53800e97e7577f1e3e328afc7fd6ccddeba03e07
George_Essmat <125737319+George2251@users.noreply.github.com>
2025-08-19 12:45:06 +0300

    Delete library directory

--- library/APP/AUTO/AUTO_config.h
+++
@@ -1,6 +0,0 @@
-#ifndef APP_AUTO_AUTO_CONFIG_H_
-#define APP_AUTO_AUTO_CONFIG_H_
-
-
-
-#endif /* APP_AUTO_AUTO_CONFIG_H_ */
--- library/APP/MAN/MAN_config.h
+++
@@ -1,6 +0,0 @@
-#ifndef APP_MAN_MAN_CONFIG_H_
-#define APP_MAN_MAN_CONFIG_H_
-
-
-
-#endif /* APP_MAN_MAN_CONFIG_H_ */
